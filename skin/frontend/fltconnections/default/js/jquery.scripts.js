445fb4c4378daf5f2ac12546e8cb16902ec32c54
akhilesh <akhileshsingh099@gmail.com>
2018-02-04 02:44:38 +0530

    Account,login,register and many more page changes
    Account,login,register and many more page changes

--- skin/frontend/fltconnections/default/js/jquery.scripts.js
+++ skin/frontend/fltconnections/default/js/jquery.scripts.js
@@ -1,5 +1,9 @@
 jQuery.noConflict();
 
 jQuery(document).ready(function() {
-
+	jQuery(document).on('click','.icon-search', function(){
+		jQuery('#header-search').toggleClass( "highlight" );
+		console.log("a");
+		return false;
+	})
 });
